cd9b2890c80d80bc655e5a33816aceba11b1b507
dalimberAndrade <dalimberpubg@gmail.com>
2026-07-23 18:36:08 -0500

    Agregar funciones para ocultar y mostrar secciones y conectar con botones

--- simulador_completo.js
+++ simulador_completo.js
@@ -11,3 +11,16 @@
 
   
 //Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
+
+//Funcion para ocultar secciones
+function ocultarSecciones() 
+{
+  let seccionParametros=document.getElementById("parametros").classList.remove("activa");
+  let seccionClientes=document.getElementById("clientes").classList.remove("activa");
+}
+//Funcion para activar seccion de parametros
+function mostrarSeccion(id) 
+{
+  ocultarSecciones();
+  let seccion=document.getElementById(id).classList.add("activa");
+}
